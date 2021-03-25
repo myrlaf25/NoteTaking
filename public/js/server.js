ce9fea9387ba58b458afb95a011c886763ceab7a
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-03-25 14:58:03 -0700

    folders

--- public/js/server.js
+++ public/js/server.js
@@ -1,10 +1,18 @@
 const express = require ("express")
 const path = require("path")
 
-cont app= express();
+const app= express();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log("App listening on port " + PORT));

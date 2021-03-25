acf20f5b102aeb7dd5306255819126c78beed5d5
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-03-25 14:43:27 -0700

    starting server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,1 +1,10 @@
 const express = require ("express")
+const path = require("path")
+
+cont app= express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+

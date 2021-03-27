3f7921747fa856e9157a23af7c11c8cf267e7584
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-03-26 21:44:32 -0700

    removed console.log

--- server.js
+++ server.js
@@ -1,83 +1,75 @@
-const express = require ("express");
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const fs = require("fs");
 const path = require("path");
 const db = require("./db/db.json");
 
-const app= express();
+const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 //HTML routes
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-})
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
-})
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
 // app.get("/assets/css/styles.css", function(req, res){
 //     res.sendFile(path.join(__dirname, "assets", "css", "styles.css"));
 // })
 //API routes
-    app.get("/api/notes", function (req, res) {
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    res.json(JSON.parse(data));
+  });
+});
 
-    fs.readFile("db/db.json", "utf8", function (err, data){
-    return err  ? console.log(err) : res.json(JSON.parse(data))
-    })
-})
+app.post("/api/notes", function (req, res) {
+  const note = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+  async function addNote() {
+    let notes = await fs.promises.readFile("./db/db.json", "utf-8");
 
+    const newNote = {
+      id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+    notes = JSON.parse(notes);
+    notes.push(newNote);
 
-  app.post("/api/notes", function(req, res){
-      const note = {
-          id: uuidv4(),
-          title: req.body.title,
-          text: req.body.text
-      };
-      async function addNote(){
-          let notes = await fs.promises.readFile("./db/db.json", "utf-8");
-          console.log(notes);
-          
-          const newNote ={id: uuidv4(), title: req.body.title, text: req.body.text};
-          notes= JSON.parse(notes)
-          notes.push(newNote);
-          
-          
-          console.log(notes);
-          res.json(notes);
-          await fs.promises.writeFile("./db/db.json", JSON.stringify(notes));
-      }
-      addNote(note).catch(function (err){
-          console.log(err);
-      });
-         
-
-  })
-
-  app.delete("/api/notes/:id", function(req, res){
-
-    async function deleteNote(){
-        notes = await fs.promises.readFile("./db/db.json", "utf-8");
-        console.log(notes);
-        const { id} = req.params;
-        notes=JSON.parse(notes)
-        const oldNote = notes.filter((note)=> note.id !== id);
-        console.log(id)
-        console.log(oldNote)
-        
-        res.json(oldNote);
-
-        await fs.promises.writeFile("./db/db.json", JSON.stringify(oldNote));
+    res.json(notes);
+    await fs.promises.writeFile("./db/db.json", JSON.stringify(notes));
   }
-        deleteNote();
+  addNote(note).catch(function (err) {
+    console.log(err);
+  });
+});
 
-    })
+app.delete("/api/notes/:id", function (req, res) {
+  async function deleteNote() {
+    notes = await fs.promises.readFile("./db/db.json", "utf-8");
 
+    const { id } = req.params;
+    notes = JSON.parse(notes);
+    const oldNote = notes.filter((note) => note.id !== id);
 
-  app.listen(PORT, () => console.log("App listening on port " + PORT));
+    res.json(oldNote);
+
+    await fs.promises.writeFile("./db/db.json", JSON.stringify(oldNote));
+  }
+  deleteNote();
+});
+
+app.listen(PORT, () => console.log("App listening on port " + PORT));
